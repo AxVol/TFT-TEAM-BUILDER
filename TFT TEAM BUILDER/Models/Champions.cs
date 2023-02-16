33ec17ef725921dac9585441ece0de594e2c2e12
AxVol <ax_vol@mail.ru>
2023-02-16 10:51:01 +0300

    crate base for MVVM and base app views

---
+++ TFT TEAM BUILDER/Models/Champions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT_TEAM_BUILDER.Models
+{
+    internal class Champions
+    {
+    }
+}
---
+++ TFT TEAM BUILDER/ViewModels/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFT_TEAM_BUILDER.Core;
+
+namespace TFT_TEAM_BUILDER.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+        public Commands allItemCommand { get; set; }
+        public Commands allChampionsCommand { get; set; }
+
+        public AllChampionsViewModel allChampionVM { get; set; }
+        public AllItemsViewModel allItemsVM { get; set; }
+
+        private object currentView;
+
+        public object CurrentView
+        {
+            get { return currentView; }
+            set
+            {
+                currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            allChampionVM = new AllChampionsViewModel();
+            allItemsVM = new AllItemsViewModel();
+
+            allChampionsCommand = new Commands(obj =>
+            {
+                CurrentView = allChampionVM;
+            });
+
+            allItemCommand = new Commands(obj =>
+            {
+                CurrentView = allItemsVM;
+            });
+        }
+    }
+}

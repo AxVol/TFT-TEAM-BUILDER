60bc6bd443008ed5819a89c30c50a61170bb9f14
AxVol <ax_vol@mail.ru>
2023-03-10 12:01:46 +0300

    refactoring

--- TFT TEAM BUILDER/Models/Team.cs
+++ TFT TEAM BUILDER/Models/Team.cs
@@ -39,41 +39,39 @@ namespace TFT_TEAM_BUILDER.Models
         public ObservableCollection<Champions> slot26 { get; set; }
         public ObservableCollection<Champions> slot27 { get; set; }
 
-        public Team(string name, ObservableCollection<Champions> previewTeam, ObservableCollection<Traits> teamTraits, ObservableCollection<Items> teamItems, ObservableCollection<Champions> slot0, ObservableCollection<Champions> slot1, ObservableCollection<Champions> slot2, ObservableCollection<Champions> slot3, ObservableCollection<Champions> slot4, ObservableCollection<Champions> slot5, ObservableCollection<Champions> slot6, ObservableCollection<Champions> slot7, ObservableCollection<Champions> slot8, ObservableCollection<Champions> slot9, ObservableCollection<Champions> slot10, ObservableCollection<Champions> slot11, ObservableCollection<Champions> slot12, ObservableCollection<Champions> slot13, ObservableCollection<Champions> slot14, ObservableCollection<Champions> slot15, ObservableCollection<Champions> slot16, ObservableCollection<Champions> slot17, ObservableCollection<Champions> slot18, ObservableCollection<Champions> slot19, ObservableCollection<Champions> slot20, ObservableCollection<Champions> slot21, ObservableCollection<Champions> slot22, ObservableCollection<Champions> slot23, ObservableCollection<Champions> slot24, ObservableCollection<Champions> slot25, ObservableCollection<Champions> slot26, ObservableCollection<Champions> slot27)
+        public Team()
         {
-            this.name = name;
-            this.previewTeam = previewTeam;
-            this.teamTraits = teamTraits;
-            this.teamItems = teamItems;
-            this.slot0 = slot0;
-            this.slot1 = slot1;
-            this.slot2 = slot2;
-            this.slot3 = slot3;
-            this.slot4 = slot4;
-            this.slot5 = slot5;
-            this.slot6 = slot6;
-            this.slot7 = slot7;
-            this.slot8 = slot8;
-            this.slot9 = slot9;
-            this.slot10 = slot10;
-            this.slot11 = slot11;
-            this.slot12 = slot12;
-            this.slot13 = slot13;
-            this.slot14 = slot14;
-            this.slot15 = slot15;
-            this.slot16 = slot16;
-            this.slot17 = slot17;
-            this.slot18 = slot18;
-            this.slot19 = slot19;
-            this.slot20 = slot20;
-            this.slot21 = slot21;
-            this.slot22 = slot22;
-            this.slot23 = slot23;
-            this.slot24 = slot24;
-            this.slot25 = slot25;
-            this.slot26 = slot26;
-            this.slot27 = slot27;
-        }
+            teamItems = new ObservableCollection<Items>();
+
+            slot0 = new ObservableCollection<Champions>();
+            slot1 = new ObservableCollection<Champions>();
+            slot2 = new ObservableCollection<Champions>();
+            slot3 = new ObservableCollection<Champions>();
+            slot4 = new ObservableCollection<Champions>();
+            slot5 = new ObservableCollection<Champions>();
+            slot6 = new ObservableCollection<Champions>();
+            slot7 = new ObservableCollection<Champions>();
+            slot8 = new ObservableCollection<Champions>();
+            slot9 = new ObservableCollection<Champions>();
+            slot10 = new ObservableCollection<Champions>();
+            slot11 = new ObservableCollection<Champions>();
+            slot12 = new ObservableCollection<Champions>();
+            slot13 = new ObservableCollection<Champions>();
+            slot14 = new ObservableCollection<Champions>();
+            slot15 = new ObservableCollection<Champions>();
+            slot16 = new ObservableCollection<Champions>();
+            slot17 = new ObservableCollection<Champions>();
+            slot18 = new ObservableCollection<Champions>();
+            slot19 = new ObservableCollection<Champions>();
+            slot20 = new ObservableCollection<Champions>();
+            slot21 = new ObservableCollection<Champions>();
+            slot22 = new ObservableCollection<Champions>();
+            slot23 = new ObservableCollection<Champions>();
+            slot24 = new ObservableCollection<Champions>();
+            slot25 = new ObservableCollection<Champions>();
+            slot26 = new ObservableCollection<Champions>();
+            slot27 = new ObservableCollection<Champions>();
+    }
 
         public void Serealize()
         {
--- TFT TEAM BUILDER/ViewModels/CreateBuildViewModel.cs
+++ TFT TEAM BUILDER/ViewModels/CreateBuildViewModel.cs
@@ -9,26 +9,28 @@ namespace TFT_TEAM_BUILDER.ViewModels
 {
     class CreateBuildViewModel : ObservableObject
     {
+        // Синглтон хэндлер для драг'н'дропа
         private static IDropTarget instance;
-        private string text;
-        private string saveText = "Название сборки";
-        private string fileName;
-        private Team team;
-
         public static IDropTarget Instance => instance = new ChromerDragDrop();
 
         public Commands TraitSortCommand { get; set; } 
         public Commands SaveTeamCommand { get; set; }
 
-        public static ObservableCollection<Champions> champions { get; set; }
-        public static ObservableCollection<Champions> sortList { get; set; }
-        public static ObservableCollection<Champions> offerList { get; set; }
+        public static ObservableCollection<Champions> champions { get; set; } // Список всех чемпионов
+        public static ObservableCollection<Champions> offerList { get; set; } // Список с отсортированными чемпионами отображаемые на вью которые могут подойти в команду
         public static ObservableCollection<Champions> ChampionTeam { get; set; }
-        public static ObservableCollection<Traits> traits { get; set; }
-        public static ObservableCollection<Traits> TeamTrait { get;set; }
-        public static ObservableCollection<Items> allItems { get; set; }
-        public static ObservableCollection<Items> itemsView { get; set; }
-        public static ObservableCollection<Items> teamItems { get; set; }
+        public static ObservableCollection<Traits> traits { get; set; } // Перки
+        public static ObservableCollection<Traits> TeamTrait { get; set; }
+        public ObservableCollection<Champions> sortList { get; set; } // Список который отображаеться на вью и производит сортировка в поиске
+        public ObservableCollection<Items> allItems { get; set; } // Предметы
+        public ObservableCollection<Items> itemsView { get; set; }
+
+        public Team team { get; set; }
+
+        // Переменные для свойств
+        private string searchText;
+        private string saveInformationText = "Название сборки";
+        private string fileName;
 
         public string saveFileName
         {
@@ -44,58 +46,30 @@ namespace TFT_TEAM_BUILDER.ViewModels
         public string SortEvent {
             get
             {
-                return text;
+                return searchText;
             }
             set
             {
-                text = value;
+                searchText = value;
 
                 sortList.Clear();
 
                 foreach (Champions champion in champions)
                 {
-                    if (champion.name.ToLower().StartsWith(text.ToLower()))
+                    if (champion.name.ToLower().StartsWith(searchText.ToLower()))
                     {
                         sortList.Add(champion);
                     }
                 }
             } 
         }
+        // Информация, для отображение после нажатия кнопки "Сохранить"
         public string Text
         {
-            get { return saveText; }
-            set { saveText = value; }
+            get { return saveInformationText; }
+            set { saveInformationText = value; }
         }
-
-        public ObservableCollection<Champions> slot0 { get; set; }
-        public ObservableCollection<Champions> slot1 { get; set; }
-        public ObservableCollection<Champions> slot2 { get; set; }
-        public ObservableCollection<Champions> slot3 { get; set; }
-        public ObservableCollection<Champions> slot4 { get; set; }
-        public ObservableCollection<Champions> slot5 { get; set; }
-        public ObservableCollection<Champions> slot6 { get; set; }
-        public ObservableCollection<Champions> slot7 { get; set; }
-        public ObservableCollection<Champions> slot8 { get; set; }
-        public ObservableCollection<Champions> slot9 { get; set; }
-        public ObservableCollection<Champions> slot10 { get; set; }
-        public ObservableCollection<Champions> slot11 { get; set; }
-        public ObservableCollection<Champions> slot12 { get; set; }
-        public ObservableCollection<Champions> slot13 { get; set; }
-        public ObservableCollection<Champions> slot14 { get; set; }
-        public ObservableCollection<Champions> slot15 { get; set; }
-        public ObservableCollection<Champions> slot16 { get; set; }
-        public ObservableCollection<Champions> slot17 { get; set; }
-        public ObservableCollection<Champions> slot18 { get; set; }
-        public ObservableCollection<Champions> slot19 { get; set; }
-        public ObservableCollection<Champions> slot20 { get; set; }
-        public ObservableCollection<Champions> slot21 { get; set; }
-        public ObservableCollection<Champions> slot22 { get; set; }
-        public ObservableCollection<Champions> slot23 { get; set; }
-        public ObservableCollection<Champions> slot24 { get; set; }
-        public ObservableCollection<Champions> slot25 { get; set; }
-        public ObservableCollection<Champions> slot26 { get; set; }
-        public ObservableCollection<Champions> slot27 { get; set; }
-
+        // Метод отвечающий за сортировку по перкам, по нажати на каждый из них
         private void TraitSort(object data)
         {
             string trait = data as string;
@@ -110,7 +84,8 @@ namespace TFT_TEAM_BUILDER.ViewModels
                 }
             }   
         }
-
+        // Метод отвечабщий за предложение чемпионов, которые могут подойти в команду, пока что алгоритм простой, предлагает
+        // всех, у кого совпадают перки, с перками команды
         public static void OfferList(Champions dropChampion)
         {
             offerList.Clear();
@@ -130,7 +105,7 @@ namespace TFT_TEAM_BUILDER.ViewModels
                 }
             }
         }
-
+        // Метод отвечающий за добавление или удаление перков из команды, действие зависит от параметра "action"
         public static void TraitTeamList(Champions champion, string action)
         {
             switch (action)
@@ -166,9 +141,12 @@ namespace TFT_TEAM_BUILDER.ViewModels
                         }
                     }
                     break;
+                default:
+                    throw new System.Exception("Не корректное действи, существует только - 'remove' для удаления и 'add' для добавления ");
             }         
         }
-
+        // Метод отвечащий за выбрасывания предметов без крафта, так как их выпадение слишком редкое и решаеться по факту, 
+        // нужны ли они, или нет
         public ObservableCollection<Items> ItemsView(ObservableCollection<Items> itemsList)
         {
             ObservableCollection<Items> items = new ObservableCollection<Items>();
@@ -188,12 +166,12 @@ namespace TFT_TEAM_BUILDER.ViewModels
         {
             sortList = new ObservableCollection<Champions>(JsonData.GetChampions());
             champions = new ObservableCollection<Champions>(JsonData.GetChampions());
+            traits = new ObservableCollection<Traits>(JsonData.GetTraits());
+            allItems = new ObservableCollection<Items>(JsonData.GetItems());
             offerList = new ObservableCollection<Champions>();
             ChampionTeam = new ObservableCollection<Champions>();
-            traits = new ObservableCollection<Traits>(JsonData.GetTraits());
             TeamTrait = new ObservableCollection<Traits>();
-            allItems = new ObservableCollection<Items>(JsonData.GetItems());
-            teamItems = new ObservableCollection<Items>();
+            team = new Team();
             itemsView = ItemsView(allItems);
 
             TraitSortCommand = new Commands(TraitSort);
@@ -204,10 +182,9 @@ namespace TFT_TEAM_BUILDER.ViewModels
                     try
                     {
                         Text = "Сохранено";
-
-                        team = new Team(saveFileName, ChampionTeam, TeamTrait, teamItems, slot0, slot1, slot2, slot3, slot4, slot5, slot6, slot7, slot8, slot9, slot10, slot11, slot12,
-                                        slot13, slot14, slot15, slot16, slot17, slot18, slot19, slot20, slot21, slot22, slot23, slot24,
-                                        slot25, slot26, slot27);
+                        team.name = saveFileName;
+                        team.teamTraits = TeamTrait;
+                        team.previewTeam = ChampionTeam;
 
                         team.Serealize();
                     }
@@ -220,36 +197,7 @@ namespace TFT_TEAM_BUILDER.ViewModels
                 {
                     Text = "Введите название";
                 }
-            });
-
-            slot0 = new ObservableCollection<Champions>();
-            slot1 = new ObservableCollection<Champions>();
-            slot2 = new ObservableCollection<Champions>();
-            slot3 = new ObservableCollection<Champions>();
-            slot4 = new ObservableCollection<Champions>();
-            slot5 = new ObservableCollection<Champions>();
-            slot6 = new ObservableCollection<Champions>();
-            slot7 = new ObservableCollection<Champions>();
-            slot8 = new ObservableCollection<Champions>();
-            slot9 = new ObservableCollection<Champions>();
-            slot10 = new ObservableCollection<Champions>();
-            slot11 = new ObservableCollection<Champions>();
-            slot12 = new ObservableCollection<Champions>();
-            slot13 = new ObservableCollection<Champions>();
-            slot14 = new ObservableCollection<Champions>();
-            slot15 = new ObservableCollection<Champions>();
-            slot16 = new ObservableCollection<Champions>();
-            slot17 = new ObservableCollection<Champions>();
-            slot18 = new ObservableCollection<Champions>();
-            slot19 = new ObservableCollection<Champions>();
-            slot20 = new ObservableCollection<Champions>();
-            slot21 = new ObservableCollection<Champions>();
-            slot22 = new ObservableCollection<Champions>();
-            slot23 = new ObservableCollection<Champions>();
-            slot24 = new ObservableCollection<Champions>();
-            slot25 = new ObservableCollection<Champions>();
-            slot26 = new ObservableCollection<Champions>();
-            slot27 = new ObservableCollection<Champions>();
+            });           
         }
     }
 }
